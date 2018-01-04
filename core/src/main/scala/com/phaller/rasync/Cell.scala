7750ebfb3fb4fd7799ae99143979003e2b48f8cd
Jan Kölzer <jth.koelzer@web.de>
2018-01-04 15:57:43 +0100

    Add priorities to tasks/cells.
    Each task that is added to the ThreadPoolExececutor by the HandlerPool is marked with a priority. High priority tasks are more likely to be executed earlier.
    
    Priorities for whenNext and whenComplete are automatically computed by a SchedulingStrategy, that the users selects.
    
    Use ForkJoinPool, if possible. (no strategy selected)

--- core/src/main/scala/com/phaller/rasync/Cell.scala
+++ core/src/main/scala/com/phaller/rasync/Cell.scala
@@ -1039,7 +1039,7 @@ private class CellImpl[K <: Key[V], V](pool: HandlerPool, val key: K, updater: U
           case _: FinalState[K, V] =>
           /* We are final already, so we ignore all incoming information. */
         }
-      })
+      }, pool.getSchedulingStrategy.calcPriority(this, otherCell))
     case _: FinalState[K, V] => /* We are final already, so we ignore all incoming information. */
   }
 
--- core/src/test/scala/com/phaller/rasync/test/pool.scala
+++ core/src/test/scala/com/phaller/rasync/test/pool.scala
@@ -3,6 +3,8 @@ package test
 
 import java.util.concurrent.ConcurrentHashMap
 
+import java.util.concurrent.CountDownLatch
+
 import org.scalatest.FunSuite
 
 import scala.concurrent.{ Await, Promise }
@@ -18,7 +20,7 @@ class PoolSuite extends FunSuite {
     while (i < 10000) {
       val p1 = Promise[Boolean]()
       val p2 = Promise[Boolean]()
-      pool.execute { () => { p1.success(true) }: Unit }
+      pool.execute({ () => { p1.success(true) }: Unit }, 0)
       pool.onQuiescent { () => p2.success(true) }
       try {
         Await.result(p2.future, 1.seconds)
@@ -43,7 +45,7 @@ class PoolSuite extends FunSuite {
         completer.cell.trigger()
         regCells.put(completer.cell, completer.cell)
         ()
-      })
+      }, 0) // TODO check priority
     }
     val fut = pool.quiescentResolveDefaults // set all (registered) cells to 1 via key.fallback
     Await.ready(fut, 5.seconds)
@@ -62,7 +64,7 @@ class PoolSuite extends FunSuite {
         val completer = CellCompleter[StringIntKey, Int]("somekey")
         regCells.put(completer.cell, completer.cell)
         ()
-      })
+      }, 0) // TODO check priority
     }
     val fut = pool.quiescentResolveDefaults // set all (registered) cells to 1 via key.fallback
     Await.ready(fut, 5.seconds)
@@ -70,4 +72,41 @@ class PoolSuite extends FunSuite {
     assert(regCells.size === 1000)
   }
 
+  test("prio") {
+    // this tests demonstrates, that high priority tasks are executed with high priority:
+    // The output should be roughly sorted.
+    val n = 99
+    val pool = new HandlerPool(2)
+    val latch = new CountDownLatch(n)
+
+    // generate a random list of priorities
+    val ps = scala.util.Random.shuffle((-n to n).toList)
+
+    // create a task for each priority
+    ps.foreach(p => {
+      pool.execute(new Runnable {
+        override def run(): Unit = {
+          println(p + "\t" + Thread.currentThread().getName)
+          latch.countDown()
+        }
+      }, p)
+    })
+    latch.await()
+    pool.shutdown()
+    println(ps)
+    /*
+    An actual test could make use of the inversion of ps the inversion of the output o.
+    If priorities are handled correctly, inv(o) <= inv(ps)
+
+
+    def inv(p):
+      s = 0
+      n = len(p)
+      for i in range(n‐1):
+          for j in range(i+1):
+              if p[i] > p[j]:
+                  s += 1
+      return s
+     */
+  }
 }
