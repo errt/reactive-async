37d5581734c11887c34aaf34a40199fa99e3e285
Jan Kölzer <jth.koelzer@web.de>
2018-11-16 17:46:15 +0100

    Adapt to OPAL develop branch.

--- core/src/main/scala/com/phaller/rasync/HandlerPool.scala
+++ core/src/main/scala/com/phaller/rasync/HandlerPool.scala
@@ -144,12 +144,12 @@ class HandlerPool(
       deregisterCompletedCells()
 
       // Find closed strongly connected components (cells)
-      val registered = this.cellsNotDone.asScala
+      val activeCells = this.cellsNotDone.asScala
         .filter(_.tasksActive())
         .asInstanceOf[Iterable[Cell[K, V]]]
 
-      if (registered.nonEmpty) {
-        val cSCCs = closedSCCs(registered, (cell: Cell[K, V]) => cell.totalCellDependencies)
+      if (activeCells.nonEmpty) {
+        val cSCCs = closedSCCs[Cell[K, V]](activeCells, (cell: Cell[K, V]) => cell.totalCellDependencies)
         cSCCs.foreach(resolveCycle)
 
         // Wait again for quiescent state. It's possible that other tasks where scheduled while
@@ -230,7 +230,7 @@ class HandlerPool(
 
         // Find closed strongly connected component (cell)
         if (activeCells.nonEmpty) {
-          val cSCCs = closedSCCs(activeCells, (cell: Cell[K, V]) => cell.totalCellDependencies)
+          val cSCCs = closedSCCs[Cell[K, V]](activeCells, (cell: Cell[K, V]) => cell.totalCellDependencies)
           cSCCs.foreach(resolveCycle)
           resolvedCycles = cSCCs.nonEmpty
         }
